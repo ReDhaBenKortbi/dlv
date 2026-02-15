d1810b3133cb85230b97b473395dacd5f9052460
ReDhaBenKortbi <ridhabenkortbi@gmail.com>
2026-02-15 22:40:49 +0000

    fix: node-fetch downgrade and private key formatting

--- netlify/functions/proxy-book.js
+++ netlify/functions/proxy-book.js
@@ -15,9 +15,11 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId,
-      privateKey: privateKey ? privateKey.replace(/\\n/g, "\n") : undefined,
-      clientEmail,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined,
     }),
   });
 }
